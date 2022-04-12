a43c31535944a6458e6be0d161b7f25b28eda2e0
이민석 <dat0802@gmail.com>
2022-04-13 02:09:26 +0900

    Feat: 상품등록 View 생성(create)

--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from products.views import OpenProductView, DetailPageView, ListView
+
 urlpatterns = [
+    path("/openproducts", OpenProductView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from products.models import Product
+from users.models import Seller
+
+class OpenProductView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            subject = data["subject"]
+            description = data["description"]
+            amount = data["amount"]
+            goal_amount = data["goal_amount"]
+            end_date = data["end_date"]
+            seller_name = data["seller"]
+
+            seller, created = Seller.objects.get_or_create(
+                name = seller_name
+            )
+
+            Product.objects.create(
+                subject = subject,
+                description = description,
+                amount = amount,
+                goal_amount = goal_amount,
+                end_date = end_date,
+                seller = seller
+            )
+
+            return JsonResponse({"message" : "SECCESS"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"message" : "KEYERROR"}, status=400)
