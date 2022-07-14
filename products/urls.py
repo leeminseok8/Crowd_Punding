980c12e5b38acbdf1817649d7f2338ced3bb151e
이민석 <dat0802@gmail.com>
2022-07-14 22:34:53 +0900

    Modify: Delete, Patch URL 변경

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,6 @@ from products.views import ProductView, ProductDetailView, FundingView
 
 urlpatterns = [
     path("/", ProductView.as_view()),
-    path("/<int:product_id>", ProductView.as_view()),
-    path("/detail/<int:product_id>", ProductDetailView.as_view()),
+    path("/<int:product_id>", ProductDetailView.as_view()),
     path("/funding", FundingView.as_view())
 ]
